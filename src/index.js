0a8d134a41066b39c9875217725470d724ba2dca
Itan Moshe <itanmoshe@gmail.com>
2023-08-15 15:54:14 +0300

    STRICTMODE

--- src/index.js
+++ src/index.js
@@ -24,13 +24,13 @@ axios.interceptors.request.use((config)=>{
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 
